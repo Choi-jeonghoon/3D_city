80271b90a72fd421708294e73a3b0eafffa0dbde
Choi-jeonghoon <jeongssi94@gmail.com>
2024-09-14 15:22:14 +0900

    Ref:오타 수정 및 백드라운드 색상 변경

--- src/components/LightComponent.jsx
+++ src/components/LightComponent.jsx
@@ -1,8 +1,13 @@
+import { Environment } from "@react-three/drei";
+
 export const LightComponent = () => {
   return (
     <>
       <directionalLight position={[1, 3, -1]} intensity={3} />
-      <ambientLight intensity={1} />
+      <ambientLight intensity={1} color="white" />
+      <Environment preset="forest" /> {/* 조명만 활용 */}
     </>
   );
 };
+
+/* (property) preset?: "apartment" | "city" | "dawn" | "forest" | "lobby" | "night" | "park" | "studio" | "sunset" | "warehouse" | undefined */
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import { SceneComponent } from "../components/SceneComponent";
-import CustomLoader from "../\bcommon/Loader";
+import CustomLoader from "../common/Loader";
 
 function HomePage() {
   return (
     <>
       <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
-        <color attach="background" args={["rgb(67, 127, 240)"]} />
+        {/* <color attach="background" args={["rgb(67, 127, 240)"]} /> */}
         <Suspense fallback={<CustomLoader />}>
           <SceneComponent />
         </Suspense>
